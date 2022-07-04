f516e7270eeec6b23acaf86a1f5b5065a9a47d1c
gyrodope69 <86244576+gyrodope69@users.noreply.github.com>
2022-07-04 17:35:26 +0530

    Updated

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
-const { dirname } = require("node:path/win32");
+const https = require("https");
+// const { dirname } = require("node:path/win32");
 
 const app = express();
 app.use(express.static("public"));
@@ -12,15 +13,37 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-  var firstName = req.body.firstname;   // we use body to because we us elements which are inside body element
-  var lastName = req.body.lastname;
-  var email = req.body.email;
-  console.log(firstName);
-  console.log(lastName);
-  console.log(email);
-});
+  const firstName = req.body.firstname; // we use body to because we us elements which are inside body element
+  const lastName = req.body.lastname;
+  const email = req.body.email;
 
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merger_feilds:{
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+  const  url="https://us9.api.mailchimp.com/3.0/lists/5466b34f60"
+  const option={
+    meathod: POST,
+    auth : "rohit:2289982aef4e65772f3deaa3fab63012-us9"
+  }
+  https.request(url,options,function(response){
+    
+  })
+});
 
 app.listen(3000, function () {
   console.log("Server isListening to port 3000");
 });
+
+// 2289982aef4e65772f3deaa3fab63012-us9
+// 5466b34f60
